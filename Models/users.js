2c8bcb63c861ad36bb16e2fbc04f35a5992b535b
dgraham742 <106363652+dgraham742@users.noreply.github.com>
2022-11-16 12:30:30 -0600

    models finished

--- Models/users.js
+++ Models/users.js
@@ -1,4 +1,42 @@
 const { Schema,model,mongoose} = require('mongoose');
 const dateFormat = require('../utils/dateFormat')
 
-cosnt 
+const UserSchema = new Schema(
+    {
+        userName: {
+            type:String,
+            required:true,
+            trim:true
+        },
+        createdBy: {
+            type:String,
+            required:true,
+            trim:true
+        },
+        createdAt:{
+            type:Date,
+            default:Date.now,
+            get:(createdAtVal) => dateFormat(createdAtVal)
+        },
+        thoughts:[
+            {
+                type:Schema.Types.ObjectId,
+                ref:'Thoughts'
+            }
+        ]
+    },
+    {
+        toJSON:{
+            virtuals:true,
+            getters:true,
+        },
+        id:false
+    }
+);
+
+//get total count of comments and replies on retrieval
+UserSchema.virtual('ThoughtCount').get(function(){
+    return this.thoughts.reduce((total,thoughts)=> total + this.thoughts.replies.length + 1.0) 
+});
+
+module.exports = mongoose.models.User || mongoose.model('User', UserSchema);
