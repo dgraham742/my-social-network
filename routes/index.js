7c3b16e356fa32d444a27ad6d6305c128442fe74
dgraham742 <106363652+dgraham742@users.noreply.github.com>
2022-11-16 13:56:03 -0600

    finished api routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
-const router = require('express').Router();
+const express = require('express');
+const mongoose = require('mongoose');
 
-const apiRoutes= require('./api');
+const app = express();
+const PORT = process.env.PORT || 3001;
 
-router.use((req,res)=> {
-    res.status(404).send('<h1>404 error</h1>')
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.use(require('./routes'));
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017//pizza-hunt', {
+    useNewUrlParser:true,
+    useUnifiedTopology:true
 });
 
-module.exports = router;
+//use this to log mongo quaries being executed
+mongoose.set('debug', true);
+
+
+app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
